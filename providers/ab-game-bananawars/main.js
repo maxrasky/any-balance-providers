3bf358b04769032d1b28dfcd2c376216069b22b2
max.rasky <maxgabbana@gmail.com>
2015-12-10 14:38:21 +0300

    Add icon, adjust error handling and some text correction

--- providers/ab-game-bananawars/main.js
+++ providers/ab-game-bananawars/main.js
@@ -2,6 +2,14 @@
  * AnyBanalance provider for http://bananawars.ru
  */
 
+var g_headers = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Charset': 'windows-1251,utf-8;q=0.7,*;q=0.3',
+  'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+  'Connection': 'keep-alive',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36',
+};
+
 function main(){
   var prefs = AnyBalance.getPreferences(),
       baseurl = 'http://bananawars.ru/',
@@ -13,20 +21,33 @@ function main(){
   checkEmpty(prefs.login, 'Введите имя');
   checkEmpty(prefs.password, 'Введите пароль');
 
+  html = AnyBalance.requestGet(baseurl, g_headers);
+
+  if(!html || AnyBalance.getLastStatusCode() > 400) {
+    AnyBalance.trace(html);
+    throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
+  }
+
   html = AnyBalance.requestPost(baseurl + 'index.php', {
     auth_name: prefs.login,
     auth_pass: prefs.password
   });
 
-  if(!html || AnyBalance.getLastStatusCode() > 400) {
-    throw new AnyBalance.Error('Ошибка при подключении к сайту провайдера! Попробуйте обновить данные позже.');
-  }
-
   if (!/logout/.test(html)) {
-    throw new AnyBalance.Error('Неверный логин или пароль!');
+    var re = /<td[^>]*>(?:(?!<td[^>]*>).)*?Забыли пароль.*?<\/td>/i,
+        error = getParam(html, null, null, re, replaceTagsAndSpaces, html_entity_decode),
+        isFatal = false;
+
+    if (error) {
+      isFatal = /Пользователь не найден|Неправильный пароль/i.test(error);
+      throw new AnyBalance.Error(error, null, isFatal);
+    }
+
+    AnyBalance.trace(html);
+    throw new AnyBalance.Error('Не удалось зайти в аккаунт. Сайт изменен?');
   }
 
-  AnyBalance.trace('Trying to find out the characteristics');
+  AnyBalance.trace('Пытаюсь получить характеристики');
 
   var searchValues = ['уровень:', 'бустеры:', 'деньги:', 'очки:', 'почта:'];
   var counters = ['level', 'booster', 'money', 'score', 'mail'];
